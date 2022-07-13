a39af04bb25c7a6decade5206b2263486a043ebc
Janisgee <100589152+Janisgee@users.noreply.github.com>
2022-07-13 13:14:07 +0800

    set up structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1
+        className="text-center
+      "
+      >
+        Hello
+      </h1>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;
